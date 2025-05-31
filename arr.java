de68febd5ee5066ca12c054d4b923d7bb21abae4
Isha Aggarwal <ishaaggarwal1207@gmail.com>
2025-05-31 14:31:20 +0530

    add new paragraph

--- arr.java
+++ arr.java
@@ -1,8 +1,8 @@
 public class arr {
     public static void main(String[] args) {
         
-        int[] marks={97,98,95 };
-        int [][] finalMarks ={ {97,98,95},{95,95,98}};
+        int[] marks={97,98,96 };
+        int [][] finalMarks ={ {97,98,96},{96 ,96,98}};
         System.out.println(finalMarks[1][1]);
         System.out.println(marks);
 
