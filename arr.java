460a74a29d87befd7658817641544c5e403256d5
Isha Aggarwal <ishaaggarwal1207@gmail.com>
2025-06-28 01:00:13 +0530

    Changing some value

--- arr.java
+++ arr.java
@@ -2,7 +2,7 @@ public class arr {
     public static void main(String[] args) {
         
         int[] marks={97,98,96 };
-        int [][] finalMarks ={ {97,98,96},{96 ,96,98}};
+        int [][] finalMarks ={ {97,98,96},{95 ,96,94}};
         System.out.println(finalMarks[1][1]);
         System.out.println(marks);
 
